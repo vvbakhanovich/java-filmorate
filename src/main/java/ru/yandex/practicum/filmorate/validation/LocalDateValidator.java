647e5ba6ecdebacbff7ec374beda2e015be907fe
vvbakhanovich <vv.bakh@yandex.ru>
2023-11-22 14:18:17 +0300

    refactor: Использовать Constants

--- src/main/java/ru/yandex/practicum/filmorate/validation/LocalDateValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/validation/LocalDateValidator.java
@@ -2,18 +2,17 @@ package ru.yandex.practicum.filmorate.validation;
 
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
+import ru.yandex.practicum.filmorate.util.Constants;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 public class LocalDateValidator implements ConstraintValidator<PastDate, LocalDate> {
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
     LocalDate date;
 
     @Override
     public void initialize(PastDate constraintAnnotation) {
         ConstraintValidator.super.initialize(constraintAnnotation);
-        date = LocalDate.parse(constraintAnnotation.date(), formatter);
+        date = LocalDate.parse(constraintAnnotation.date(), Constants.DATE_FORMATTER);
     }
 
     @Override
