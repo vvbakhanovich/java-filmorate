a1302ab517de587dd4af8ad33bf15cd1ab662127
vvbakhanovich <vv.bakh@yandex.ru>
2023-11-22 16:53:29 +0300

    fix: Исправить формат даты по умолчанию

--- src/main/java/ru/yandex/practicum/filmorate/validation/PastDate.java
+++ src/main/java/ru/yandex/practicum/filmorate/validation/PastDate.java
@@ -11,7 +11,7 @@ import java.lang.annotation.*;
 @Constraint(validatedBy = LocalDateValidator.class)
 public @interface PastDate {
 
-    String date() default "28/12/1895";
+    String date() default "1895-12-28";
 
     String message() default "Введите более позднюю дату.";
 
