9e5ff28ea6dc9ad517a403cc68106a227ade5fda
vvbakhanovich <vv.bakh@yandex.ru>
2023-11-22 16:52:09 +0300

    feat: Изменить отображаемое сообщение

--- src/main/java/ru/yandex/practicum/filmorate/validation/LocalDateValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/validation/LocalDateValidator.java
@@ -8,15 +8,19 @@ import java.time.LocalDate;
 
 public class LocalDateValidator implements ConstraintValidator<PastDate, LocalDate> {
     LocalDate date;
+    String message;
 
     @Override
     public void initialize(PastDate constraintAnnotation) {
         ConstraintValidator.super.initialize(constraintAnnotation);
         date = LocalDate.parse(constraintAnnotation.date(), Constants.DATE_FORMATTER);
+        message = constraintAnnotation.message();
     }
 
     @Override
     public boolean isValid(LocalDate localDate, ConstraintValidatorContext constraintValidatorContext) {
+        constraintValidatorContext.disableDefaultConstraintViolation();
+        constraintValidatorContext.buildConstraintViolationWithTemplate(message).addConstraintViolation();
 
         return localDate.isAfter(date);
     }
