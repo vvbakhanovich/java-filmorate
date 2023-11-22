524b7903e01b700bb57de31dfb724539422380b4
vvbakhanovich <vv.bakh@yandex.ru>
2023-11-22 10:30:47 +0300

    feat: Реализовать UserController

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,11 +1,53 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.dto.FilmDto;
+import ru.yandex.practicum.filmorate.dto.UserDto;
+import ru.yandex.practicum.filmorate.exception.FilmNotFoundException;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
-@RequestMapping("/films")
+@RequestMapping("/users")
 public class UserController {
 
+    private long userId = 1;
+    Map<Long, User> users = new HashMap<>();
 
+    @PostMapping
+    public ResponseEntity<User> addUser(@RequestBody UserDto userDto) {
+        User user = User.build(generateId(), userDto.getEmail(), userDto.getLogin(), userDto.getName(),
+                userDto.getBirthday());
+        users.put(user.getUserId(), user);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@RequestBody UserDto updatedUserDto, @RequestParam long userId) {
+        User storedUser = users.get(userId);
+        if (storedUser != null) {
+            storedUser.setEmail(updatedUserDto.getEmail());
+            storedUser.setLogin(updatedUserDto.getLogin());
+            storedUser.setName(updatedUserDto.getName());
+            storedUser.setBirthday(updatedUserDto.getBirthday());
+            return ResponseEntity.ok(storedUser);
+        } else {
+            throw new UserNotFoundException("Фильм с id " + userId + " не найден.");
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<ArrayList<User>> getAllUser() {
+        return ResponseEntity.ok(new ArrayList<>(users.values()));
+    }
+
+    private long generateId() {
+        return userId++;
+    }
 }
