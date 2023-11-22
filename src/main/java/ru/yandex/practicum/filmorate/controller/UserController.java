83c361fd5840ca16beffbca4e4c5c76f46be8847
vvbakhanovich <vv.bakh@yandex.ru>
2023-11-22 14:07:57 +0300

    feat: Добавить логирование

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +15,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
+@Slf4j
 public class UserController {
 
     private long userId = 1;
@@ -24,10 +26,11 @@ public class UserController {
         User user = User.build(generateId(), userDto.getEmail(), userDto.getLogin(), userDto.getName(),
                 userDto.getBirthday());
         users.put(user.getUserId(), user);
+        log.info("Добавление нового пользователя: " + user);
         return new ResponseEntity<>(user, HttpStatus.CREATED);
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("/{userId}")
     public ResponseEntity<User> updateUser(@PathVariable long userId, @Valid @RequestBody UserDto updatedUserDto) {
         User storedUser = users.get(userId);
         if (storedUser != null) {
@@ -35,14 +38,17 @@ public class UserController {
             storedUser.setLogin(updatedUserDto.getLogin());
             storedUser.setName(updatedUserDto.getName());
             storedUser.setBirthday(updatedUserDto.getBirthday());
+            log.info("Обновление пользователя с id " + userId + ": " + storedUser);
             return ResponseEntity.ok(storedUser);
         } else {
-            throw new UserNotFoundException("Фильм с id " + userId + " не найден.");
+            log.warn("Пользователь с id " + userId + " не был найден.");
+            throw new UserNotFoundException("Пользователь с id " + userId + " не найден.");
         }
     }
 
     @GetMapping
     public ResponseEntity<ArrayList<User>> getAllUser() {
+        log.info("Получение списка всех пользователей.");
         return ResponseEntity.ok(new ArrayList<>(users.values()));
     }
 
