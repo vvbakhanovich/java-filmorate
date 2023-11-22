85f7780838a4c51468fd035019c4d79c26a79481
vvbakhanovich <vv.bakh@yandex.ru>
2023-11-22 10:03:52 +0300

    feat: Создать UserController

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,11 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/films")
+public class UserController {
+
+
+}
