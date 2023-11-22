740dc69be75027496c06e9f801b00e332d7e3664
vvbakhanovich <vv.bakh@yandex.ru>
2023-11-22 10:13:06 +0300

    feat: Создать ApplicationExceptionHandler

---
+++ src/main/java/ru/yandex/practicum/filmorate/handler/ApplicationExceptionHandler.java
@@ -0,0 +1,16 @@
+package ru.yandex.practicum.filmorate.handler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.yandex.practicum.filmorate.exception.FilmNotFoundException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ApplicationExceptionHandler {
+
+
+}
