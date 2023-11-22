e6200207c66ba6c914a858bffc95f5774f7f3cb8
vvbakhanovich <vv.bakh@yandex.ru>
2023-11-22 17:34:05 +0300

    test: Создать метод валидации dto

---
+++ src/test/java/ru/yandex/practicum/filmorate/validation/ValidationTestUtils.java
@@ -0,0 +1,21 @@
+package ru.yandex.practicum.filmorate.validation;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.Set;
+
+public class ValidationTestUtils {
+    private static final Validator VALIDATOR = Validation.buildDefaultValidatorFactory().getValidator();
+
+    private ValidationTestUtils() {
+
+    }
+
+    public static <T> boolean dtoHasErrorMessage(T dto, @NotNull String message) {
+        Set<ConstraintViolation<T>> errors = VALIDATOR.validate(dto);
+        return errors.stream().map(ConstraintViolation::getMessage).anyMatch(message::equals);
+    }
+}
