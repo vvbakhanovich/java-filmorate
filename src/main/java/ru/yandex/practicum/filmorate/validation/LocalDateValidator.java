eb857e5e5a4683b056b6c2d465b59e7634abb769
vvbakhanovich <vv.bakh@yandex.ru>
2023-11-22 13:11:02 +0300

    feat: Создать LocalDateValidator

---
+++ src/main/java/ru/yandex/practicum/filmorate/validation/LocalDateValidator.java
@@ -0,0 +1,14 @@
+package ru.yandex.practicum.filmorate.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.time.LocalDate;
+
+public class LocalDateValidator implements ConstraintValidator<PastDate, LocalDate> {
+
+    @Override
+    public boolean isValid(LocalDate localDate, ConstraintValidatorContext constraintValidatorContext) {
+        return localDate.isAfter(LocalDate.of(1895, 12, 28));
+    }
+}
