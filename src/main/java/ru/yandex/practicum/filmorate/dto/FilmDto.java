c48d1ae05074cf5a0bb7cf04d454f439af39d2d9
vvbakhanovich <vv.bakh@yandex.ru>
2023-11-22 13:10:37 +0300

    feat: Добавить валидацию

--- src/main/java/ru/yandex/practicum/filmorate/dto/FilmDto.java
+++ src/main/java/ru/yandex/practicum/filmorate/dto/FilmDto.java
@@ -1,15 +1,23 @@
 package ru.yandex.practicum.filmorate.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import ru.yandex.practicum.filmorate.validation.PastDate;
 
 import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
 public class FilmDto {
+    @NotBlank(message = "Название не может быть пустым.")
     private String name; //название
+    @Size(max = 200, message = "Максимальная длина описания: 200 символов")
     private String description; //описание
+    @PastDate
     private LocalDate releaseDate; //дата релиза
+    @Positive(message = "Продолжительность должна быть больше нуля")
     private int duration; //продолжительность фильма
 }
