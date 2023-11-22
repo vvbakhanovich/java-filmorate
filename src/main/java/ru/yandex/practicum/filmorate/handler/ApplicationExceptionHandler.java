f7348b5e758498fd64848da62dad8d899a87392b
vvbakhanovich <vv.bakh@yandex.ru>
2023-11-22 13:09:33 +0300

    feat: Добавить обработку ошибок валидации

--- src/main/java/ru/yandex/practicum/filmorate/handler/ApplicationExceptionHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/handler/ApplicationExceptionHandler.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.handler;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -16,6 +17,19 @@ public class ApplicationExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Map<String, String> handleFilmNotFoundException(FilmNotFoundException e) {
         Map<String, String> exceptions = new HashMap<>();
+        exceptions.put("errorMessage", e.getLocalizedMessage());
+        return exceptions;
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleInvalidException(MethodArgumentNotValidException e) {
+        Map<String, String> exceptions = new HashMap<>();
+        e.getBindingResult().getFieldErrors().forEach(fieldError -> {
+                    exceptions.put(fieldError.getField(), fieldError.getDefaultMessage());
+                }
+        );
+
         exceptions.put("errorMessage", e.getMessage());
         return exceptions;
     }
