87435477d932cd0f45cb38ec7a8ea0b84f0b0d12
vvbakhanovich <vv.bakh@yandex.ru>
2023-11-22 09:42:11 +0300

    feat: Создать User

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,14 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class User {
+    private long userId; //целочисленный идентификатор
+    private String email; //электронная почта
+    private String login; //логин пользователя
+    private String name; //имя для отображения
+    private LocalDate birthday; //дата рождения
+}
