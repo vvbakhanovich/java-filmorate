f9238d464d54c2a83df0ca8a42f3482a4bd2cd2b
vvbakhanovich <vv.bakh@yandex.ru>
2023-11-22 13:45:55 +0300

    feat: Добавить инициализацию параметра date

--- src/main/java/ru/yandex/practicum/filmorate/validation/LocalDateValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/validation/LocalDateValidator.java
@@ -4,11 +4,21 @@ import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class LocalDateValidator implements ConstraintValidator<PastDate, LocalDate> {
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    LocalDate date;
+
+    @Override
+    public void initialize(PastDate constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+        date = LocalDate.parse(constraintAnnotation.date(), formatter);
+    }
 
     @Override
     public boolean isValid(LocalDate localDate, ConstraintValidatorContext constraintValidatorContext) {
-        return localDate.isAfter(LocalDate.of(1895, 12, 28));
+
+        return localDate.isAfter(date);
     }
 }
