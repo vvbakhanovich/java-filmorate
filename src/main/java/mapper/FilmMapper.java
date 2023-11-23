22aa1eb16a59e90586b6c2723c47aec9f7914c8d
vvbakhanovich <vv.bakh@yandex.ru>
2023-11-23 19:31:19 +0300

    feat: Создать FilmMapper

---
+++ src/main/java/mapper/FilmMapper.java
@@ -0,0 +1,20 @@
+package mapper;
+
+import ru.yandex.practicum.filmorate.dto.FilmDto;
+import ru.yandex.practicum.filmorate.model.Film;
+
+public final class FilmMapper {
+    private FilmMapper() {
+
+    }
+
+    public static FilmDto toDto(Film film) {
+        return new FilmDto(film.getId(), film.getName(), film.getDescription(), film.getReleaseDate(),
+                film.getDuration());
+    }
+
+    public static Film toModel(FilmDto filmDto) {
+        return new Film(filmDto.getId(), filmDto.getName(), filmDto.getDescription(), filmDto.getReleaseDate(),
+                filmDto.getDuration());
+    }
+}
