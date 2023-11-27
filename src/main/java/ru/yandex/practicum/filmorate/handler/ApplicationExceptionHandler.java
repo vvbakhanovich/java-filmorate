0ef273872593d3e11cd1feaca8837b8aa2ddbb30
vvbakhanovich <vv.bakh@yandex.ru>
2023-11-27 14:17:38 +0300

    feat: Уровень логирования error

--- src/main/java/ru/yandex/practicum/filmorate/handler/ApplicationExceptionHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/handler/ApplicationExceptionHandler.java
@@ -20,6 +20,7 @@ public class ApplicationExceptionHandler {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public Map<String, String> handleUserNotFoundException(NotFoundException e) {
         Map<String, String> exceptions = new HashMap<>();
+        log.error("Объект не найден! " + e.getLocalizedMessage());
         exceptions.put("errorMessage", e.getLocalizedMessage());
         return exceptions;
     }
@@ -30,7 +31,7 @@ public class ApplicationExceptionHandler {
         Map<String, String> exceptions = new HashMap<>();
         for (FieldError error : e.getBindingResult().getFieldErrors()) {
             exceptions.put(error.getField(), error.getDefaultMessage());
-            log.warn("Поле " + error.getField() + " не прошло валидацию. Причина: " + error.getDefaultMessage());
+            log.error("Поле " + error.getField() + " не прошло валидацию. Причина: " + error.getDefaultMessage());
         }
 
         return exceptions;
