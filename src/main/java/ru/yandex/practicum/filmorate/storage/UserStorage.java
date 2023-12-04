f259d45d4be1aab3dbabbaf888b94a1fa69e3153
vvbakhanovich <vv.bakh@yandex.ru>
2023-12-04 17:13:29 +0300

    feat: Создать интерфейс UserStorage

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -0,0 +1,9 @@
+package ru.yandex.practicum.filmorate.storage;
+
+import ru.yandex.practicum.filmorate.model.User;
+
+public interface UserStorage {
+    long addUser(User user);
+    boolean removeUser(long userId);
+    boolean updateUser(User user);
+}
