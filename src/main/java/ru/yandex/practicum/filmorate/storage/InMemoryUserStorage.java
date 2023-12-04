7b39ff361cebe1a3755c00a9ac40483485188606
vvbakhanovich <vv.bakh@yandex.ru>
2023-12-04 17:14:09 +0300

    feat: Реализовать InMemoryUserStorage

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryUserStorage.java
@@ -0,0 +1,30 @@
+package ru.yandex.practicum.filmorate.storage;
+
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class InMemoryUserStorage implements UserStorage {
+    private final Map<Long, User> users = new HashMap<>();
+
+    @Override
+    public long addUser(final User user) {
+        users.put(user.getId(), user);
+        return user.getId();
+    }
+
+    @Override
+    public boolean removeUser(long user) {
+        return users.remove(user) != null;
+    }
+
+    @Override
+    public boolean updateUser(final User updateUser) {
+        if (users.containsKey(updateUser.getId())) {
+            users.put(updateUser.getId(), updateUser);
+            return true;
+        }
+        return false;
+    }
+}
