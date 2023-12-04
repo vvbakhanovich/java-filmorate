a4ffa91930ae04df33a10bced1dc27489f9dc03d
vvbakhanovich <vv.bakh@yandex.ru>
2023-12-04 17:13:57 +0300

    feat: Реализовать InMemoryFilmStorage

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryFilmStorage.java
@@ -0,0 +1,31 @@
+package ru.yandex.practicum.filmorate.storage;
+
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class InMemoryFilmStorage implements FilmStorage {
+
+    private final Map<Long, Film> films = new HashMap<>();
+
+    @Override
+    public long addFilm(final Film film) {
+        films.put(film.getId(), film);
+        return film.getId();
+    }
+
+    @Override
+    public boolean removeFilm(long filmId) {
+        return films.remove(filmId) != null;
+    }
+
+    @Override
+    public boolean updateFilm(final Film updatedFilm) {
+        if (films.containsKey(updatedFilm.getId())) {
+            films.put(updatedFilm.getId(), updatedFilm);
+            return true;
+        }
+        return false;
+    }
+}
