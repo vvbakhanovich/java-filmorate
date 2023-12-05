0e1081ebacd81aa6d81b575adb6ff0963edb0ed9
vvbakhanovich <vv.bakh@yandex.ru>
2023-12-05 13:45:23 +0300

    feat: Добавить поле userService

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -2,67 +2,42 @@ package ru.yandex.practicum.filmorate.controller;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.dto.UserDto;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
-import ru.yandex.practicum.filmorate.mapper.UserMapper;
-import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.UserService;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.Collection;
 
 @RestController
 @RequestMapping("/users")
 @Slf4j
 public class UserController {
 
-    private long userId = 1;
-    private final Map<Long, User> users = new HashMap<>();
+    UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public UserDto addUser(@Valid @RequestBody UserDto userDto) {
-        User user = new User(generateId(), userDto.getEmail(), userDto.getLogin(), checkIfNameIsEmpty(userDto),
-                userDto.getBirthday());
-        users.put(user.getId(), user);
-        log.info("Добавление нового пользователя: " + user);
-        return UserMapper.toDto(user);
+        return userService.addUser(userDto);
     }
 
     @PutMapping
     @ResponseStatus(HttpStatus.OK)
     public UserDto updateUser(@Valid @RequestBody UserDto updatedUserDto) {
-        long userId = updatedUserDto.getId();
-        User storedUser = users.get(userId);
-        if (storedUser != null) {
-            storedUser.setEmail(updatedUserDto.getEmail());
-            storedUser.setLogin(updatedUserDto.getLogin());
-            storedUser.setName(checkIfNameIsEmpty(updatedUserDto));
-            storedUser.setBirthday(updatedUserDto.getBirthday());
-            log.info("Обновление пользователя с id " + userId + ": " + storedUser);
-            return UserMapper.toDto(storedUser);
-        } else {
-            log.error("Пользователь с id " + userId + " не был найден.");
-            throw new NotFoundException("Пользователь с id " + userId + " не найден.");
-        }
+        return updateUser(updatedUserDto);
     }
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public ArrayList<UserDto> getAllUser() {
-        log.info("Получение списка всех пользователей.");
-        return users.values().stream().map(UserMapper::toDto).collect(Collectors.toCollection(ArrayList::new));
-    }
-
-    private long generateId() {
-        return userId++;
-    }
-
-    private String checkIfNameIsEmpty(UserDto userDto) {
-        return userDto.getName() == null || userDto.getName().isBlank() ? userDto.getLogin() : userDto.getName();
+    public Collection<UserDto> getAllUser() {
+        return getAllUser();
     }
 }
