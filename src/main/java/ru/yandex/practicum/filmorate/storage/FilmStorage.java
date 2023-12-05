ccac9827f9d5f5672c6490bf2a87a7ca9159596a
vvbakhanovich <vv.bakh@yandex.ru>
2023-12-05 13:44:32 +0300

    feat: Наследовать Storage<T>

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -2,10 +2,5 @@ package ru.yandex.practicum.filmorate.storage;
 
 import ru.yandex.practicum.filmorate.model.Film;
 
-public interface FilmStorage {
-    long addFilm(Film film);
-
-    boolean removeFilm(long filmId);
-
-    boolean updateFilm(Film film);
+public interface FilmStorage extends Storage<Film> {
 }
