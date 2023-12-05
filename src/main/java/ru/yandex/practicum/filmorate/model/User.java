63c87bf907253cf21a23024569d1142849417cd3
vvbakhanovich <vv.bakh@yandex.ru>
2023-12-05 13:45:02 +0300

    feat: Добавить поле friends

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @AllArgsConstructor
@@ -13,4 +15,5 @@ public class User {
     private String login; //логин пользователя
     private String name; //имя для отображения
     private LocalDate birthday; //дата рождения
+    private final Set<Long> friends = new HashSet<>(); //список друзей
 }
