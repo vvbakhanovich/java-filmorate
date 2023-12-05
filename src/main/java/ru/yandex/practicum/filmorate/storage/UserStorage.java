75a875731344871a5c8ec559e8fbe6e9522524e5
vvbakhanovich <vv.bakh@yandex.ru>
2023-12-05 13:44:25 +0300

    feat: Наследовать Storage<T>

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -2,10 +2,5 @@ package ru.yandex.practicum.filmorate.storage;
 
 import ru.yandex.practicum.filmorate.model.User;
 
-public interface UserStorage {
-    long addUser(User user);
-
-    boolean removeUser(long userId);
-
-    boolean updateUser(User user);
+public interface UserStorage extends Storage<User> {
 }
