3eb01da7b03d72f5f11a2fc07701cd89c20cf6ea
vvbakhanovich <vv.bakh@yandex.ru>
2023-12-05 10:07:20 +0300

    style: checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -1,10 +1,11 @@
 package ru.yandex.practicum.filmorate.storage;
 
-import ru.yandex.practicum.filmorate.dto.UserDto;
 import ru.yandex.practicum.filmorate.model.Film;
 
 public interface FilmStorage {
     long addFilm(Film film);
+
     boolean removeFilm(long filmId);
+
     boolean updateFilm(Film film);
 }
