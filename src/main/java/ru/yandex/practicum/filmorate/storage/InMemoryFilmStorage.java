77e5bc336ee1b92df41bbf589b78ab60efbeff31
vvbakhanovich <vv.bakh@yandex.ru>
2023-12-05 10:10:11 +0300

    feat: Добавить поле IdGenerator

--- src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryFilmStorage.java
@@ -1,27 +1,37 @@
 package ru.yandex.practicum.filmorate.storage;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class InMemoryFilmStorage implements FilmStorage {
+@Repository
+public class InMemoryFilmStorage implements Storage<Film> {
+
+    IdGenerator<Long> idGenerator;
+
+    @Autowired
+    public InMemoryFilmStorage(IdGenerator<Long> idGenerator) {
+        this.idGenerator = idGenerator;
+    }
 
     private final Map<Long, Film> films = new HashMap<>();
 
     @Override
-    public long addFilm(final Film film) {
-        films.put(film.getId(), film);
+    public long add(final Film film) {
+        films.put(idGenerator.generateId(), film);
         return film.getId();
     }
 
     @Override
-    public boolean removeFilm(final long filmId) {
+    public boolean remove(final long filmId) {
         return films.remove(filmId) != null;
     }
 
     @Override
-    public boolean updateFilm(final Film updatedFilm) {
+    public boolean update(final Film updatedFilm) {
         if (films.containsKey(updatedFilm.getId())) {
             films.put(updatedFilm.getId(), updatedFilm);
             return true;
