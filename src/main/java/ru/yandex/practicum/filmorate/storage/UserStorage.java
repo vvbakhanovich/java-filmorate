92edbcadca868cda5cc576c7c0ed19052b28e4b1
vvbakhanovich <vv.bakh@yandex.ru>
2023-12-05 10:07:30 +0300

    style: checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -4,6 +4,8 @@ import ru.yandex.practicum.filmorate.model.User;
 
 public interface UserStorage {
     long addUser(User user);
+
     boolean removeUser(long userId);
+
     boolean updateUser(User user);
 }
