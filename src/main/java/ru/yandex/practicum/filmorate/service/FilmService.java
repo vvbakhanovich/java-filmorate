b514f3b1ddba6f5614ef79953a30c79cc3cf81d9
vvbakhanovich <vv.bakh@yandex.ru>
2023-12-06 15:42:24 +0300

    refactor: Убрать выбрасывание исключений, поля приватные финальные

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -1,13 +1,11 @@
 package ru.yandex.practicum.filmorate.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.dto.FilmDto;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.mapper.FilmMapper;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.FilmStorage;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
 
@@ -20,18 +18,13 @@ import static ru.yandex.practicum.filmorate.mapper.FilmMapper.toDto;
 import static ru.yandex.practicum.filmorate.mapper.FilmMapper.toModel;
 
 @Service
+@RequiredArgsConstructor
 @Slf4j
 public class FilmService {
 
-    FilmStorage filmStorage;
+    private final FilmStorage filmStorage;
 
-    UserStorage userStorage;
-
-    @Autowired
-    public FilmService(FilmStorage filmStorage, UserStorage userStorage) {
-        this.filmStorage = filmStorage;
-        this.userStorage = userStorage;
-    }
+    private final UserStorage userStorage;
 
     public FilmDto addFilm(FilmDto filmDto) {
         Film film = toModel(filmDto);
@@ -43,13 +36,9 @@ public class FilmService {
     public FilmDto updateFilm(FilmDto updatedFilmDto) {
         Film updatedFilm = toModel(updatedFilmDto);
         long filmId = updatedFilmDto.getId();
-        if (filmStorage.update(updatedFilm)) {
-            log.info("Обновление фильма с id {}: {}", filmId, updatedFilm);
-            return toDto(updatedFilm);
-        } else {
-            log.error("Фильм с id {} не был найден.", filmId);
-            throw new NotFoundException("Фильм с id " + filmId + " не найден.");
-        }
+        filmStorage.update(updatedFilm);
+        log.info("Обновление фильма с id {}: {}", filmId, updatedFilm);
+        return toDto(updatedFilm);
     }
 
     public Collection<FilmDto> getAllFilms() {
@@ -59,49 +48,24 @@ public class FilmService {
 
     public FilmDto getFilmById(long filmId) {
         Film film = filmStorage.findById(filmId);
-        if (film != null) {
-            log.info("Фильм с id {} найден.", filmId);
-            return toDto(film);
-        } else {
-            log.error("Фильм с id {} не был найден.", filmId);
-            throw new NotFoundException("Фильм id " + filmId + " не найден.");
-        }
+        log.info("Фильм с id {} найден.", filmId);
+        return toDto(film);
     }
 
     public FilmDto likeFilm(long filmId, long userId) {
         Film film = filmStorage.findById(filmId);
-        User user = userStorage.findById(userId);
-        if (film != null) {
-            if (user != null) {
-                film.getLikes().add(userId);
-                log.info("Пользователь с id {} поставил лайк фильму с id {}", userId, filmId);
-                return toDto(film);
-            } else {
-                log.error("Пользователь с id {} не был найден.", userId);
-                throw new NotFoundException("Пользователь id " + userId + " не найден");
-            }
-        } else {
-            log.error("Фильм с id {} не был найден.", filmId);
-            throw new NotFoundException("Пользователь id " + userId + " не найден.");
-        }
+        userStorage.findById(userId);
+        film.getLikes().add(userId);
+        log.info("Пользователь с id {} поставил лайк фильму с id {}", userId, filmId);
+        return toDto(film);
     }
 
     public FilmDto removeLike(long filmId, long userId) {
         Film film = filmStorage.findById(filmId);
-        User user = userStorage.findById(userId);
-        if (film != null) {
-            if (user != null) {
-                film.getLikes().remove(userId);
-                log.info("Пользователь с id {} удалил лайк фильма с id {}", userId, filmId);
-                return toDto(film);
-            } else {
-                log.error("Пользователь с id {} не был найден.", userId);
-                throw new NotFoundException("Пользователь id " + userId + " не найден");
-            }
-        } else {
-            log.error("Фильм с id {} не был найден.", filmId);
-            throw new NotFoundException("Пользователь id " + userId + " не найден.");
-        }
+        userStorage.findById(userId);
+        film.getLikes().remove(userId);
+        log.info("Пользователь с id {} удалил лайк фильма с id {}", userId, filmId);
+        return toDto(film);
     }
 
     public Collection<FilmDto> getMostPopularFilms(int count) {
--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -1,10 +1,9 @@
 package ru.yandex.practicum.filmorate.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.dto.UserDto;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.mapper.UserMapper;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
@@ -16,15 +15,11 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 @Slf4j
 public class UserService {
 
-    UserStorage userStorage;
-
-    @Autowired
-    public UserService(UserStorage userStorage) {
-        this.userStorage = userStorage;
-    }
+    private final UserStorage userStorage;
 
     public UserDto addUser(final UserDto userDto) {
         User user = UserMapper.toModel(validateUserName(userDto));
@@ -36,13 +31,9 @@ public class UserService {
     public UserDto updateUser(final UserDto updatedUserDto) {
         User updatedUser = UserMapper.toModel(validateUserName(updatedUserDto));
         long userId = updatedUser.getId();
-        if (userStorage.update(updatedUser)) {
-            log.info("Обновление пользователя с id {} : {}", userId, updatedUser);
-            return UserMapper.toDto(updatedUser);
-        } else {
-            log.error("Пользователь с id {} не был найден.", userId);
-            throw new NotFoundException("Пользователь с id " + userId + " не найден.");
-        }
+        userStorage.update(updatedUser);
+        log.info("Обновление пользователя с id {} : {}", userId, updatedUser);
+        return UserMapper.toDto(updatedUser);
     }
 
     public Collection<UserDto> getAllUser() {
@@ -52,93 +43,55 @@ public class UserService {
 
     public UserDto getUserById(long userId) {
         User user = userStorage.findById(userId);
-        if (user != null) {
-            log.info("Пользователь с id {} найден.", userId);
-            return UserMapper.toDto(user);
-        } else {
-            log.error("Пользователь с id {} не был найден.", userId);
-            throw new NotFoundException("Пользователь id " + userId + " не найден.");
-        }
+        log.info("Пользователь с id {} найден.", userId);
+        return UserMapper.toDto(user);
     }
 
     public UserDto addFriend(final Long userId, long friendId) {
         User user = userStorage.findById(userId);
         User friend = userStorage.findById(friendId);
-        if (user != null) {
-            if (friend != null) {
-                user.getFriends().add(friendId);
-                friend.getFriends().add(userId);
-                log.info("Пользователи с id {} и id {} стали друзьями.", friendId, userId);
-                return UserMapper.toDto(user);
-            } else {
-                log.error("Пользователь с id {} не был найден.", friendId);
-                throw new NotFoundException("Пользователь id " + friendId + " не найден");
-            }
-        } else {
-            log.error("Пользователь с id {} не был найден.", userId);
-            throw new NotFoundException("Пользователь id " + userId + " не найден.");
-        }
+        user.getFriends().add(friendId);
+        friend.getFriends().add(userId);
+        log.info("Пользователи с id {} и id {} стали друзьями.", friendId, userId);
+        return UserMapper.toDto(user);
+
     }
 
     public Collection<UserDto> showFriendList(long userId) {
         User user = userStorage.findById(userId);
-        if (user != null) {
-            Set<Long> friendIds = user.getFriends();
-            List<User> result = new ArrayList<>();
-            for (Long friendId : friendIds) {
-                result.add(userStorage.findById(friendId));
-            }
-            log.info("Список друзей пользователся с id {}: {}", userId, result);
-            return result.stream().map(UserMapper::toDto).collect(Collectors.toList());
-        } else {
-            log.error("Пользователь с id {} не был найден.", userId);
-            throw new NotFoundException("Пользователь id " + userId + " не найден.");
+        Set<Long> friendIds = user.getFriends();
+        List<User> result = new ArrayList<>();
+        for (Long friendId : friendIds) {
+            result.add(userStorage.findById(friendId));
         }
+        log.info("Список друзей пользователся с id {}: {}", userId, result);
+        return result.stream().map(UserMapper::toDto).collect(Collectors.toList());
     }
 
     public Collection<UserDto> findCommonFriends(long userId, long otherUserId) {
         User user = userStorage.findById(userId);
         User otherUser = userStorage.findById(otherUserId);
-        if (user != null) {
-            if (otherUser != null) {
-                Set<Long> userFriendsId = user.getFriends();
-                Set<Long> otherUserFriendsId = otherUser.getFriends();
-                Set<Long> commonIds = userFriendsId.stream().filter(otherUserFriendsId::contains).collect(Collectors.toSet());
-                List<User> result = new ArrayList<>();
-                log.info("Cписок id общих друзей пользователей с id {} и {}: {}", userId, otherUser, result);
-                for (Long id : commonIds) {
-                    result.add(userStorage.findById(id));
-                }
-                return result.stream().map(UserMapper::toDto).collect(Collectors.toList());
-            } else {
-                log.error("Пользователь с id {} не был найден.", otherUser);
-                throw new NotFoundException("Пользователь id " + otherUser + " не найден");
-            }
-        } else {
-            log.error("Пользователь с id {} не был найден.", userId);
-            throw new NotFoundException("Пользователь id " + userId + " не найден.");
+
+        Set<Long> userFriendsId = user.getFriends();
+        Set<Long> otherUserFriendsId = otherUser.getFriends();
+        Set<Long> commonIds = userFriendsId.stream().filter(otherUserFriendsId::contains).collect(Collectors.toSet());
+        List<User> result = new ArrayList<>();
+        log.info("Cписок id общих друзей пользователей с id {} и {}: {}", userId, otherUser, result);
+        for (Long id : commonIds) {
+            result.add(userStorage.findById(id));
         }
+        return result.stream().map(UserMapper::toDto).collect(Collectors.toList());
     }
 
     public UserDto removeFriend(long userId, long friendId) {
         User user = userStorage.findById(userId);
         User friend = userStorage.findById(friendId);
-        if (user != null) {
-            if (friend != null) {
-                Set<Long> userFriendsId = user.getFriends();
-                Set<Long> otherUserFriendsId = friend.getFriends();
-                userFriendsId.remove(friendId);
-                otherUserFriendsId.remove(userId);
-                log.info("Пользователи с id {} и {} перестали быть друзьями", userId, friend);
-                return UserMapper.toDto(user);
-            } else {
-                log.error("Пользователь с id {} не был найден.", friendId);
-                throw new NotFoundException("Пользователь id " + friendId + " не найден");
-            }
-        } else {
-            log.error("Пользователь с id {} не был найден.", userId);
-            throw new NotFoundException("Пользователь id " + userId + " не найден.");
-        }
+        Set<Long> userFriendsId = user.getFriends();
+        Set<Long> otherUserFriendsId = friend.getFriends();
+        userFriendsId.remove(friendId);
+        otherUserFriendsId.remove(userId);
+        log.info("Пользователи с id {} и {} перестали быть друзьями", userId, friend);
+        return UserMapper.toDto(user);
     }
 
     private UserDto validateUserName(final UserDto userDto) {
