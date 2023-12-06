bd910335bd36aa4b5fdf71ed7748f5131ca46dd9
vvbakhanovich <vv.bakh@yandex.ru>
2023-12-06 15:53:51 +0300

    feat: Поставить аннотацию UtilityClass

--- src/main/java/ru/yandex/practicum/filmorate/util/Constants.java
+++ src/main/java/ru/yandex/practicum/filmorate/util/Constants.java
@@ -1,11 +1,11 @@
 package ru.yandex.practicum.filmorate.util;
 
+import lombok.experimental.UtilityClass;
+
 import java.time.format.DateTimeFormatter;
 
+@UtilityClass
 public final class Constants {
-    private Constants() {
-
-    }
 
     public static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
