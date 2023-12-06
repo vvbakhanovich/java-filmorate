f5414089e14cc49050fffa11725a84e676805d03
vvbakhanovich <vv.bakh@yandex.ru>
2023-12-06 11:26:52 +0300

    feat: Поставить аннотации Component и Scope

--- src/main/java/ru/yandex/practicum/filmorate/storage/LongIdGenerator.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/LongIdGenerator.java
@@ -1,5 +1,10 @@
 package ru.yandex.practicum.filmorate.storage;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("prototype")
 public class LongIdGenerator implements IdGenerator<Long> {
 
     long id = 1;
