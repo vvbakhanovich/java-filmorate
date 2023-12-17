e3d94d5bc3f9804ea5ab890d04cc17a46772321e
vvbakhanovich <vv.bakh@yandex.ru>
2023-12-17 13:33:24 +0300

    refactor: Переместить создание бина в FilmorateApplication

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -1,13 +1,27 @@
 package ru.yandex.practicum.filmorate;
 
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import ru.yandex.practicum.filmorate.util.Constants;
 
 @SpringBootApplication
 public class FilmorateApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FilmorateApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FilmorateApplication.class, args);
+    }
 
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
+        return builder -> {
+
+            builder.deserializers(new LocalDateDeserializer(Constants.DATE_FORMATTER));
+
+            builder.serializers(new LocalDateSerializer(Constants.DATE_FORMATTER));
+        };
+    }
 }
