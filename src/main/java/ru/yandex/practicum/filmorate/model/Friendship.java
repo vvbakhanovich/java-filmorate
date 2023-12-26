d7598b5a910876c1f69808b94cc650b4e250a905
vvbakhanovich <vv.bakh@yandex.ru>
2023-12-26 10:47:09 +0300

    refactor: Преобразовать в enum

--- src/main/java/ru/yandex/practicum/filmorate/model/Friendship.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Friendship.java
@@ -1,12 +1,28 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
-@Data
-@AllArgsConstructor
-public class Friendship {
-    private Long userId;
-    private Long friendId;
-    private FriendshipStatus friendshipStatus;
+@RequiredArgsConstructor
+public enum Friendship {
+    ACK(1, "Acknowledged"),
+    NOT_ACK(2, "Not acknowledged");
+
+    @Getter
+    private final Integer statusId;
+    private final String status;
+
+    @Override
+    public String toString() {
+        return status;
+    }
+
+    public static Friendship fromString(String s) {
+        for (Friendship fs : Friendship.values()) {
+            if (fs.status.contains(s)) {
+                return fs;
+            }
+        }
+        throw new IllegalArgumentException("Статуса дружбы '" + s + "' не существует.");
+    }
 }
