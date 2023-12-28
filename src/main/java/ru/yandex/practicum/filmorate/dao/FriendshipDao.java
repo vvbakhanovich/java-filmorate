f94fb654753bb48372589123dec224b0ca37e911
vvbakhanovich <vv.bakh@yandex.ru>
2023-12-28 17:32:29 +0300

    refactor: Удалить неиспользуемый метод

--- src/main/java/ru/yandex/practicum/filmorate/dao/FriendshipDao.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/FriendshipDao.java
@@ -1,15 +1,9 @@
 package ru.yandex.practicum.filmorate.dao;
 
-import ru.yandex.practicum.filmorate.model.Friendship;
-
-import java.util.Map;
-
 public interface FriendshipDao {
     void add(long userId, long friendId, int statusId);
 
     void remove(long userId, long friendId);
-
-    Map<Long, Friendship> findById(long userId);
 
     void update(long userId, long friendId, int statusId);
 }
--- src/main/java/ru/yandex/practicum/filmorate/dao/db/FriendshipDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/db/FriendshipDbStorage.java
@@ -2,16 +2,9 @@ package ru.yandex.practicum.filmorate.dao.db;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.dao.FriendshipDao;
-import ru.yandex.practicum.filmorate.model.Friendship;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 @Repository
 @Slf4j
@@ -29,37 +22,15 @@ public class FriendshipDbStorage implements FriendshipDao {
     @Override
     public void remove(long userId, long friendId) {
         final String sql = "DELETE FROM friendship WHERE user_id = ? AND friend_id = ?";
-        int rows =  jdbcTemplate.update(sql, userId, friendId);
+        final int rows = jdbcTemplate.update(sql, userId, friendId);
         if (rows != 1) {
             log.info("Удаление из друзей не было произведено, так как {} не был в списке друзей у {}", friendId, userId);
         }
     }
 
     @Override
-    public Map<Long, Friendship> findById(final long userId) {
-//        final String friendsSql = "SELECT f.friend_id, fs.status_name FROM friendship f LEFT JOIN friendship_status fs " +
-//                "ON f.friendship_status_id = fs.id WHERE f.user_id = ?";
-//        Map<Long, Friendship> friends = jdbcTemplate.query(friendsSql, this::extractToFriendStatusMap, userId);
-//        if (friends == null) {
-//            friends = Collections.emptyMap();
-//        }
-//        return friends;
-        return null;
-    }
-
-    @Override
     public void update(long userId, long friendId, int statusId) {
         final String sql = "UPDATE friendship SET friendship_status_id = ? WHERE user_id = ? AND friend_id = ?";
         jdbcTemplate.update(sql, statusId, userId, friendId);
-    }
-
-    private Map<Long, Friendship> extractToFriendStatusMap(ResultSet rs) throws SQLException, DataAccessException {
-        Map<Long, Friendship> result = new LinkedHashMap<>();
-        while (rs.next()) {
-            Long friendId = rs.getLong("friend_id");
-            String friendshipStatus = rs.getString("status_name");
-//            result.put(friendId, Friendship.fromString(friendshipStatus));
-        }
-        return result;
     }
 }
