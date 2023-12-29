7eb75c579d0c0576a6de4bd7a94000bb2c62d0f1
vvbakhanovich <vv.bakh@yandex.ru>
2023-12-29 14:04:14 +0300

    feat: Создать findAll

--- src/main/java/ru/yandex/practicum/filmorate/dao/FilmLikeDao.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/FilmLikeDao.java
@@ -1,9 +1,13 @@
 package ru.yandex.practicum.filmorate.dao;
 
+import java.util.Map;
+
 public interface FilmLikeDao {
     void add(long filmId, long userId);
 
     void update(long filmId, long userId);
 
     Long getCountById(long filmId);
+
+    Map<Long, Long> findAll();
 }
