95460a1db235d429bd50208e5758cf25962837fb
vvbakhanovich <vv.bakh@yandex.ru>
2023-12-29 12:40:42 +0300

    refactor: Изменить getAllById

--- src/main/java/ru/yandex/practicum/filmorate/dao/db/FilmGenreDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/db/FilmGenreDbStorage.java
@@ -18,7 +18,7 @@ public class FilmGenreDbStorage implements FilmGenreDao {
 
     @Override
     public void add(final long filmId, final long genreId) {
-        final String sql = "MERGE INTO film_genre (film_id, genre_id) VALUES (?, ?)";
+        final String sql = "INSERT INTO film_genre (film_id, genre_id) VALUES (?, ?)";
         jdbcTemplate.update(sql, filmId, genreId);
     }
 
@@ -30,11 +30,12 @@ public class FilmGenreDbStorage implements FilmGenreDao {
 
     @Override
     public List<Genre> getAllById(long filmId) {
-        final String sql = "SELECT genre_id FROM film_genre WHERE film_id = ?";
+        final String sql = "SELECT fg.genre_id g.genre_name FROM film_genre fg JOIN genre g ON fg.genre_id = g.id" +
+                " WHERE film_id = ?";
         return jdbcTemplate.query(sql, this::mapRowToLong, filmId);
     }
 
     private Genre mapRowToLong(ResultSet rs, int rowNum) throws SQLException {
-        return Genre.fromId(rs.getInt("genre_id"));
+        return new Genre(rs.getInt("genre_id"), rs.getString("genre_name"));
     }
 }
