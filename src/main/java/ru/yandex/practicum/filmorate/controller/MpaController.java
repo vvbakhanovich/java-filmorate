360ef24abf085b43ada3377e618e8c297dd22d84
vvbakhanovich <vv.bakh@yandex.ru>
2023-12-30 14:03:24 +0300

    feat: Создать MpaController

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/MpaController.java
@@ -0,0 +1,29 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.filmorate.model.Mpa;
+import ru.yandex.practicum.filmorate.service.MpaService;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/mpa")
+@RequiredArgsConstructor
+public class MpaController {
+
+    private final MpaService mpaService;
+
+    @GetMapping
+    public Collection<Mpa> getAll() {
+        return mpaService.findAll();
+    }
+
+    @GetMapping("/{mpaId}")
+    public Mpa getMpaById(@PathVariable int mpaId) {
+        return mpaService.findMpaById(mpaId);
+    }
+}
