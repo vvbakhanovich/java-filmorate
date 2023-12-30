6c6f4a94ab4aea88f9c236cc7fe5fec048259ac0
vvbakhanovich <vv.bakh@yandex.ru>
2023-12-30 14:29:44 +0300

    feat: Добавить логгер

--- src/main/java/ru/yandex/practicum/filmorate/service/impl/GenreServiceImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/impl/GenreServiceImpl.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.dao.GenreDao;
 import ru.yandex.practicum.filmorate.model.Genre;
@@ -10,17 +11,20 @@ import java.util.Collection;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class GenreServiceImpl implements GenreService {
 
     private final GenreDao genreDao;
 
     @Override
     public Genre findById(int id) {
+        log.info("Запрошен жанр с id '{}'.", id);
         return genreDao.findGenreById(id);
     }
 
     @Override
     public Collection<Genre> findAll() {
+        log.info("Запрошен список всех жанров.");
         return genreDao.findAll();
     }
 }
