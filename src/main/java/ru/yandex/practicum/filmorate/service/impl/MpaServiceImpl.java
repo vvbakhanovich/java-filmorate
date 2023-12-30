db395369913f557da64ce32365ec32949a713715
vvbakhanovich <vv.bakh@yandex.ru>
2023-12-30 14:28:59 +0300

    feat: Добавить логгер

--- src/main/java/ru/yandex/practicum/filmorate/service/impl/MpaServiceImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/impl/MpaServiceImpl.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.dao.MpaDao;
 import ru.yandex.practicum.filmorate.model.Mpa;
@@ -10,17 +11,20 @@ import java.util.Collection;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class MpaServiceImpl implements MpaService {
 
     private final MpaDao mpaDao;
 
     @Override
     public Mpa findMpaById(int id) {
+        log.info("Запрошен Mpa с id '{}'.", id);
         return mpaDao.findById(id);
     }
 
     @Override
     public Collection<Mpa> findAll() {
+        log.info("Запрошен список всех Mpa.");
         return mpaDao.findAll();
     }
 }
