bfba59156bd13162bd71ec47d35272b47b6b5b4b
vvbakhanovich <vv.bakh@yandex.ru>
2024-01-01 17:10:34 +0300

    javadoc

--- src/main/java/ru/yandex/practicum/filmorate/service/impl/MpaServiceImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/impl/MpaServiceImpl.java
@@ -16,12 +16,23 @@ public class MpaServiceImpl implements MpaService {
 
     private final MpaStorage mpaStorage;
 
+    /**
+     * Поиск рейтинга MPA по идентификатору.
+     *
+     * @param id идентификатор рейтинга
+     * @return рейтинг MPA
+     */
     @Override
     public Mpa findMpaById(int id) {
         log.info("Запрошен Mpa с id '{}'.", id);
         return mpaStorage.findById(id);
     }
 
+    /**
+     * Поиск всех рейтингов MPA
+     *
+     * @return список всех рейтингов, хранящихся в БД.
+     */
     @Override
     public Collection<Mpa> findAll() {
         log.info("Запрошен список всех Mpa.");
