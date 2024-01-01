65ce9b3dc729413e1ffea690e8b2e7f9f866b978
vvbakhanovich <vv.bakh@yandex.ru>
2024-01-01 15:19:26 +0300

    refactor: Удалить метод update, delete

--- src/main/java/ru/yandex/practicum/filmorate/dao/FilmGenreStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/FilmGenreStorage.java
@@ -7,11 +7,7 @@ import java.util.List;
 public interface FilmGenreStorage {
     void add(long filmId, long genreId);
 
-    void update(long filmId, long genreId);
-
     List<Genre> findAllById(long filmId);
 
-    void deleteAll(long id);
-
-    void delete(long filmId, long genreId);
+    void deleteAllById(long id);
 }
--- src/main/java/ru/yandex/practicum/filmorate/dao/impl/db/FilmGenreDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/impl/db/FilmGenreDbStorage.java
@@ -23,13 +23,6 @@ public class FilmGenreDbStorage implements FilmGenreStorage {
     }
 
     @Override
-    public void update(final long filmId, final long genreId) {
-        deleteAll(filmId);
-        final String sql = "INSERT INTO film_genre (film_id, genre_id) VALUES (?, ?)";
-        jdbcTemplate.update(sql, filmId, genreId);
-    }
-
-    @Override
     public List<Genre> findAllById(long filmId) {
         final String sql = "SELECT fg.genre_id, g.genre_name FROM film_genre fg JOIN genre g ON fg.genre_id = g.id" +
                 " WHERE film_id = ?";
@@ -37,15 +30,9 @@ public class FilmGenreDbStorage implements FilmGenreStorage {
     }
 
     @Override
-    public void deleteAll(long id) {
+    public void deleteAllById(long id) {
         final String sql = "DELETE FROM film_genre WHERE film_id = ?";
         jdbcTemplate.update(sql, id);
-    }
-
-    @Override
-    public void delete(long filmId, long genreId) {
-        final String sql = "DELETE FROM film_genre WHERE film_id = ? AND genre_id = ?";
-        jdbcTemplate.update(sql, filmId, genreId);
     }
 
     private Genre mapRowToLong(ResultSet rs, int rowNum) throws SQLException {
