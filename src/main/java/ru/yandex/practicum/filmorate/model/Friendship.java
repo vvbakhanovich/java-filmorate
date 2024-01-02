3de112bce00b989ef72bc3e395eae6217a703274
vvbakhanovich <vv.bakh@yandex.ru>
2024-01-02 21:20:24 +0300

    style:

--- src/main/java/ru/yandex/practicum/filmorate/model/Friendship.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Friendship.java
@@ -10,6 +10,5 @@ import lombok.NoArgsConstructor;
 public class Friendship {
 
     private Long id;
-
     private String status;
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/Mpa.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Mpa.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Mpa {
+
     private int id;
     private String name;
 }
