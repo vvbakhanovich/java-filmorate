438a40ad391cf6c5668e16666fca29cc5375d5a9
vvbakhanovich <vv.bakh@yandex.ru>
2024-01-03 14:34:17 +0300

    feat: Использовать Dto

--- src/main/java/ru/yandex/practicum/filmorate/controller/GenreController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/GenreController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.yandex.practicum.filmorate.model.Genre;
+import ru.yandex.practicum.filmorate.dto.GenreDto;
 import ru.yandex.practicum.filmorate.service.GenreService;
 
 import java.util.Collection;
@@ -18,12 +18,12 @@ public class GenreController {
     private final GenreService genreService;
 
     @GetMapping
-    public Collection<Genre> getAll() {
+    public Collection<GenreDto> getAll() {
         return genreService.findAll();
     }
 
     @GetMapping("/{genreId}")
-    public Genre getGenreById(@PathVariable int genreId) {
+    public GenreDto getGenreById(@PathVariable int genreId) {
         return genreService.findById(genreId);
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/MpaController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/MpaController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.yandex.practicum.filmorate.model.Mpa;
+import ru.yandex.practicum.filmorate.dto.MpaDto;
 import ru.yandex.practicum.filmorate.service.MpaService;
 
 import java.util.Collection;
@@ -18,12 +18,12 @@ public class MpaController {
     private final MpaService mpaService;
 
     @GetMapping
-    public Collection<Mpa> getAll() {
+    public Collection<MpaDto> getAll() {
         return mpaService.findAll();
     }
 
     @GetMapping("/{mpaId}")
-    public Mpa getMpaById(@PathVariable int mpaId) {
+    public MpaDto getMpaById(@PathVariable int mpaId) {
         return mpaService.findMpaById(mpaId);
     }
 }
