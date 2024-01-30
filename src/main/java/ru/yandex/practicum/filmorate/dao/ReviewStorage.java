7d8f437dff003bd7529284407e96c75aff47a725
vvbakhanovich <vv.bakh@yandex.ru>
2024-01-30 14:00:49 +0300

    feat: Реализовать getReviewsByFilmId

--- src/main/java/ru/yandex/practicum/filmorate/dao/ReviewStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/ReviewStorage.java
@@ -2,5 +2,8 @@ package ru.yandex.practicum.filmorate.dao;
 
 import ru.yandex.practicum.filmorate.model.Review;
 
-public interface ReviewStorage extends Dao<Review>{
+import java.util.List;
+
+public interface ReviewStorage extends Dao<Review> {
+    List<Review> findByFilmIdLimitBy(long filmId, int count);
 }
