747fc306aafec7dd3f68084a9a86bed184202cd4
vvbakhanovich <vv.bakh@yandex.ru>
2024-01-30 22:13:25 +0300

    refactor: Не использовать дополнительные поля

--- src/main/java/ru/yandex/practicum/filmorate/model/ReviewLike.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/ReviewLike.java
@@ -1,17 +1,6 @@
 package ru.yandex.practicum.filmorate.model;
 
 public enum ReviewLike {
-    LIKE("like"),
-    DISLIKE("dislike");
-
-    private final String type;
-
-    ReviewLike(String type) {
-        this.type = type;
-    }
-
-    @Override
-    public String toString() {
-        return type;
-    }
+    LIKE,
+    DISLIKE;
 }
