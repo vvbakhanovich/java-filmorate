439a1b1f65f13b8988c665b623afde8d9fa03ed6
vvbakhanovich <vv.bakh@yandex.ru>
2024-01-30 09:03:55 +0300

    feat: Создать ReviewMapper

---
+++ src/main/java/ru/yandex/practicum/filmorate/mapper/ReviewMapper.java
@@ -0,0 +1,31 @@
+package ru.yandex.practicum.filmorate.mapper;
+
+import lombok.experimental.UtilityClass;
+import ru.yandex.practicum.filmorate.dto.ReviewDto;
+import ru.yandex.practicum.filmorate.model.Review;
+
+@UtilityClass
+public class ReviewMapper {
+
+    public static ReviewDto toDto(Review review) {
+        return ReviewDto.builder()
+                .reviewId(review.getReviewId())
+                .content(review.getContent())
+                .isPositive(review.isPositive())
+                .useful(review.getUseful())
+                .filmId(review.getFilmId())
+                .userId(review.getUserId())
+                .build();
+    }
+
+    public static Review toModel(ReviewDto reviewDto) {
+        return Review.builder()
+                .reviewId(reviewDto.getReviewId())
+                .content(reviewDto.getContent())
+                .isPositive(reviewDto.isPositive())
+                .useful(reviewDto.getUseful())
+                .filmId(reviewDto.getFilmId())
+                .userId(reviewDto.getUserId())
+                .build();
+    }
+}
