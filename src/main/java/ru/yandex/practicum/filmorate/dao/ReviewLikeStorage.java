f64bd8d98ad957e0a07d5f0a4b0662fd0465d37c
vvbakhanovich <vv.bakh@yandex.ru>
2024-01-30 22:14:01 +0300

    refactor: Добавить параметр reviewId в delete

--- src/main/java/ru/yandex/practicum/filmorate/dao/ReviewLikeStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/ReviewLikeStorage.java
@@ -3,5 +3,5 @@ package ru.yandex.practicum.filmorate.dao;
 public interface ReviewLikeStorage {
     void add(long reviewId, long userId, String type);
 
-    void delete(long userId, String type);
+    void delete(long reviewId, long userId, String type);
 }
