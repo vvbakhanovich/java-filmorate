eb17e6293d93aa3940b4cf68a1c797c4ec9c9bd1
vvbakhanovich <vv.bakh@yandex.ru>
2024-01-31 20:42:58 +0300

    feat: Добавить метод findGenresInIdList

--- src/main/java/ru/yandex/practicum/filmorate/dao/impl/FilmGenreDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/impl/FilmGenreDbStorage.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.dao.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -10,7 +11,7 @@ import ru.yandex.practicum.filmorate.model.Genre;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.List;
+import java.util.*;
 
 @Repository
 @RequiredArgsConstructor
@@ -29,6 +30,16 @@ public class FilmGenreDbStorage implements FilmGenreStorage {
         final String sql = "SELECT fg.genre_id, g.genre_name FROM film_genre fg JOIN genre g ON fg.genre_id = g.id" +
                 " WHERE film_id = ?";
         return jdbcTemplate.query(sql, this::mapRowToLong, filmId);
+    }
+
+    @Override
+    public Map<Long, List<Genre>> findGenresInIdList(Set<Long> filmIds) {
+        final String ids = String.join(",", Collections.nCopies(filmIds.size(), "?"));
+        final String sql = String.format(
+                "SELECT fg.film_id, fg.genre_id, g.genre_name FROM film_genre fg JOIN genre g ON fg.genre_id = g.id" +
+                        " WHERE fg.film_id IN (%s)", ids);
+
+        return jdbcTemplate.query(sql, this::extractToMap, filmIds.toArray());
     }
 
     @Override
@@ -56,5 +67,20 @@ public class FilmGenreDbStorage implements FilmGenreStorage {
 
     private Genre mapRowToLong(ResultSet rs, int rowNum) throws SQLException {
         return new Genre(rs.getInt("genre_id"), rs.getString("genre_name"));
+    }
+
+    private Map<Long, List<Genre>> extractToMap(ResultSet rs) throws SQLException, DataAccessException {
+        final Map<Long, List<Genre>> filmIdGenreMap = new HashMap<>();
+        while (rs.next()) {
+            final Long filmId = rs.getLong(1);
+            List<Genre> genres = filmIdGenreMap.get(filmId);
+            if (genres == null) {
+                genres = new ArrayList<>();
+            }
+            final Genre genre = new Genre(rs.getInt("genre_id"), rs.getString("genre_name"));
+            genres.add(genre);
+            filmIdGenreMap.put(filmId, genres);
+        }
+        return filmIdGenreMap;
     }
 }
