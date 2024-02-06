a53a21b4393fa58c3cd993182bb5a61c9a06c0ea
Elena <elena-bokhan@yandex.ru>
2024-02-06 10:40:21 +0400

    feat: added feed upd

--- src/main/java/ru/yandex/practicum/filmorate/dao/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/UserStorage.java
@@ -8,7 +8,6 @@ import java.util.Collection;
 public interface UserStorage extends Dao<User> {
 
     Collection<User> findFriendsByUserId(long userId);
-
     Collection<User> findCommonFriends(long userId, long anotherUserId);
     Collection<Feed> getFeed(long id);
 }
