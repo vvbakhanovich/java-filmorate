37240146634150ea2d86b1c17daddd5d3cb9371b
vvbakhanovich <vv.bakh@yandex.ru>
2024-02-10 15:23:24 +0300

    feat: Использовать mergre при добавлении оценки фильму

--- src/main/java/ru/yandex/practicum/filmorate/dao/impl/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/impl/FilmDbStorage.java
@@ -223,7 +223,7 @@ public class FilmDbStorage implements FilmStorage {
 
     @Override
     public void addLikeToFilm(final long filmId, final long userId, final int rating) {
-        final String sql = "INSERT INTO film_like (film_id, user_id, rating) VALUES (?, ?, ?)";
+        final String sql = "MERGE INTO film_like (film_id, user_id, rating) VALUES (?, ?, ?)";
         jdbcTemplate.update(sql, filmId, userId, rating);
     }
 
